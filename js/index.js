ea79aac52b4b1ce364e4d6fe2bd67666f7d97e5b
Prakhar Gupta <prakgup20@gmail.com>
2023-02-12 04:35:23 +0530

    heroInfo complete

--- js/index.js
+++ js/index.js
@@ -35,7 +35,7 @@ function showData(json){
 
     let heroImg = document.createElement('img');
     let anchorTag = document.createElement('a');
-    anchorTag.setAttribute('href','hero.html');
+    // anchorTag.setAttribute('href','hero.html');
     let path = hero.thumbnail.path +'.'+ hero.thumbnail.extension;
     heroImg.setAttribute("src",path);
     heroImg.setAttribute("width","100%");
@@ -54,7 +54,75 @@ function showData(json){
   }
 }
 
-function addHeroId(heroId){
+async function addHeroId(heroId){
   console.log(heroId);
-  myLocalStorage.setItem('heroId',heroId);
+  let url = `https://gateway.marvel.com:443/v1/public/characters/${heroId}?apikey=${apiKey}&hash=${hash}&ts=1`;
+  console.log(url);
+  await  fetch(url)
+          .then((response) => response.json())
+          .then((data) => showHeroInfo(data));
+}
+
+async function showHeroInfo(json){
+  let hero = json.data.results[0];
+  // console.log(hero);
+  let path = hero.thumbnail.path +'.'+ hero.thumbnail.extension;
+  let comics = hero.comics.available;
+  let series = hero.series.available;
+  let events = hero.events.available;
+  let stories = hero.stories.available;
+
+  let description = '';
+  if(hero.description){
+    description = hero.description;
+  }else{
+    description = hero.name;
+  }
+  
+  let list = document.getElementById('heroList');
+  list.innerHTML = '';
+  list.innerHTML += `
+            <div class="hero-info-box">
+              
+              <div class="img-about">
+                  <img src ="${path}" width="40%" height="65%" display="inline">
+                  <div style="width:50%;">
+                    <h1>${hero.name}</h1>
+                    <label>About:</label>
+                    <p>${description}</p>
+                    <label>Comics:</label> ${comics} |
+                    <label>Series:</label> ${series} |
+                    <label>Events:</label> ${events} |
+                    <label>Stories:</label> ${stories}
+                  </div>
+              </div>
+              
+              <div id="comics"> 
+              
+              </div>
+            </div>
+  `;
+  let comicUrl = `https://gateway.marvel.com:443/v1/public/characters/${hero.id}/comics?apikey=${apiKey}&hash=${hash}&ts=1`;
+  await  fetch(comicUrl)
+  .then((response) => response.json())
+  .then((data) => loadComics(data.data.results));
+}
+
+async function loadComics(comics){
+  console.log(comics);
+  let comicsDiv = document.getElementById("comics");
+  for(let i = 0; i<6; i++){
+    let comic = comics[i];
+    if(comic){
+      console.log(comic);
+      let comicCard = document.createElement('div');
+      comicCard.setAttribute("class","hero-box");
+      let path = comic.thumbnail.path +'.'+ comic.thumbnail.extension;
+      comicCard.innerHTML = `
+        <h4>${comic.title}</h4>
+        <img src=${path} width="100%" height="100%">
+      `;
+      comicsDiv.appendChild(comicCard);
+    }
+  }
 }
